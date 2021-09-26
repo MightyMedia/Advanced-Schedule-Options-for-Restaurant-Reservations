7a4dc1f16f3a532447c79ee695145e57af3dd24d
Bas van den Wijngaard <bas@mightymedia.nl>
2021-09-26 21:49:44 +0200

    Temp dates file?

---
+++ dates.js
@@ -0,0 +1,43 @@
+var rtb_pickadate = {
+	"date_format": "dd-mm-yyyy",
+	"time_format": "h:i",
+	"disable_dates": [
+		7, 
+		2, 
+		4, 
+		5, 
+		6, 
+		["2020", 0, "20"], 
+		["2020", 1, "10"], 
+		["2020", 1, "12"], 
+		["2020", 1, "24"], 
+		["2020", 1, "26"], 
+		["2020", 2, "11"], 
+		["2020", 2, "18"], 
+		["2020", 2, "25"]
+	],
+	"schedule_open": [
+		{ 
+			"weekdays": { 
+				"monday": "1", 
+				"wednesday": "1" } 
+		}
+	],
+	"schedule_closed": [
+		{ "date": "2020\/01\/20" }, 
+		{ "date": "2020\/02\/10" }, 
+		{ "date": "2020\/02\/12" }, 
+		{ "date": "2020\/02\/24" }, 
+		{ "date": "2020\/02\/26" }, 
+		{ "date": "2020\/03\/11" }, 
+		{ "date": "2020\/03\/18" }, 
+		{ "date": "2020\/03\/25" }
+	],
+	"early_bookings": null,
+	"late_bookings": "same_day",
+	"enable_max_reservations": null,
+	"date_onload": "soonest",
+	"time_interval": "15",
+	"first_day": "1",
+	"allow_past": ""
+};
